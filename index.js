cfd956bde1b5600d39f504bc874b03745462cb5e
mustafaince <50414471+mustafaince@users.noreply.github.com>
2025-05-11 14:06:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,5 +14,4 @@ app.listen(3000, () => {
     console.log("listening on port 3000");
 });
 
-// forms, (orm = sequelize = mysql), admin, mongodb, mongoose, membership, restapi, tokenbased auth, publish
 
